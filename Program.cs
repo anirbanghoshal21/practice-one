dfb2e1c7ad7bd9f2b89aac9b76d46fb120133639
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-08-17 16:33:27 +0530

    Second program

--- Program.cs
+++ Program.cs
@@ -1,46 +1,29 @@
-﻿using System;//base class library--- parent
+﻿using System;
 
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            int i = 0, j = 0, k = 0, userchoice;
-            char ch = '+';
-            Console.WriteLine("Menu \n 1.Multiplication \n 2.Division \n 3.Exit");
-            userchoice = Convert.ToInt32(Console.ReadLine());
-            if (userchoice != 3)
-            {
-                Console.WriteLine("Enter value for i");
-                i = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter value for j");
-                j = Convert.ToInt32(Console.ReadLine());
-            }
+            int i=10;
+            Int32 j=20;
+            float pi = 3.14f;
+            int k = i + j;
+            Console.WriteLine(k);
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Enter i for division");
+            i =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter j for division");
+            j =Convert.ToInt32(Console.ReadLine());
+            k= i / j;
+            Console.WriteLine("The division of i and j" + k);
+            Console.WriteLine("The division of {0} and {1}= {2}" ,i,j,k);
 
-            switch (userchoice)
-            {
-                case 1:
-                    ch = '*';
-                    k = i * j;
-                    break;
-                case 2:
-                    ch = '/';
-                    k = i / j;
-                    break;
-                case 3:
-                    Environment.Exit(1);
-                    break;
-                default:
-                    Environment.Exit(1);
-                    break;
-            }
-            Console.WriteLine("The answer of {0} {1} {2}={3}", i, ch, j, k);
-
-            Console.ReadLine();//Read()/ ReadKey()
-            //comment
-
+            Console.WriteLine("Hello");
+            Console.WriteLine(DateTime.Now);
+            Console.ReadLine();
 
         }
     }
